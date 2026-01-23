4c2d7558041748c91d9dab3d1e00fb22e65a4cdf
JosefStack <jojohnson350@gmail.com>
2026-01-23 16:13:38 +0200

    listings-test-1

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(express.static("public"));
 // const API_KEY = "2a0c596eb4f144d694d7523b43f55440";
 
 const API_URL = "https://api.coingecko.com/api/v3/";
-const MARKET_DATA_ENDPOINT = "coins/markets"
+const MARKET_DATA_ENDPOINT = "coins/markets";
+const TRENDING_ENDPOINT = "search/trending";
 
 const API_KEY = "CG-VbxmU8iAp28HBYTRGfi6UN3g";
 
@@ -31,62 +32,49 @@ const config = {
     }
 }
 
-async function test() {
-    const URL = API_URL + MARKET_DATA_ENDPOINT;
+
+app.get("/", async (req, res) => {
+    const URL = API_URL + TRENDING_ENDPOINT;
     console.log(URL);
+    let coins;
+
     try {
         const response = await axios.get(URL, config);
-
-        const result = JSON.stringify(response.data);
-        console.log(result);
-        fs.writeFile("marketdata.json", result, (err) => {
+        const coinData = response.data.coins;
+        fs.writeFile("testdata.json", JSON.stringify(coinData), (err) => {
             if (err) throw err;
             else {
-                console.log("market data saved.")
+                console.log("data written.")
             }
         })
+
+
+
+        coins = coinData.map(coin => ({
+                id: coin.item.id,
+                symbol: coin.item.symbol,
+                name: coin.item.name,
+                image: coin.item.thumb,
+                current_price: coin.item.data.price,
+                // market_cap_rank: coin.item.market_cap_rank,
+                // high_24h: coin.item.high_24h,
+                // low_24h: coin.item.low_24h,
+                // price_change_24h: coin.item.price_change_24h,
+                price_change_percentage: coin.item.data.price_change_percentage_24h.usd,
+                // last_updated: coin.item.last_updated
+            }));
+        
+        
     }
     catch (error) {
         console.log(`Request failed: ${JSON.stringify(error.response.data)}`)
     }
 
-}
-// test();
-
-let coins;
-function readJSON() {
-
-    fs.readFile("marketdata.json", "utf-8", (err, data) => {
-        if (err) throw err;
-        else {
-            const marketData = JSON.parse(data);
-
-            coins = marketData.map(coin => ({
-                id: coin.id,
-                symbol: coin.symbol,
-                name: coin.name,
-                image: coin.image,
-                current_price: coin.current_price,
-                market_cap_rank: coin.market_cap_rank,
-                high_24h: coin.high_24h,
-                low_24h: coin.low_24h,
-                price_change_24h: coin.price_change_24h,
-                price_change_percentage: coin.price_change_percentage_24h,
-                last_updated: coin.last_updated
-            }));
-
-            console.log(coins);
-        }
-
-    })
-
-}
-readJSON();
 
 
-app.get("/", (req, res) => {
     res.render("index.ejs", {
-        page: "home"
+        page: "home",
+        coins: coins.slice(0, 5),
     });
 })
 
